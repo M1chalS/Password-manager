38196628ed19894d072f1bbbcc6b3a2640730e97
M1chalS <michalszajner@wp.pl>
2023-05-15 15:14:16 +0200

    Added get all and create user routes

---
+++ password-manager/app/Http/Controllers/UserAccountController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserAccountController extends Controller
+{
+    public function index()
+    {
+        $user = User::all();
+
+        return response()->json($user, 200);
+    }
+    
+    public function store(Request $request)
+    {
+        $user = User::create($request->validate([
+            'name' => 'required|string',
+            'last_name' => 'required|string',
+            'email' => 'required|unique:users',
+            'password' => 'required',
+            'is_admin' => 'boolean|optional'
+        ]));
+
+        return response()->json($user, 201);
+    }
+}
--- password-manager/app/Models/User.php
+++ password-manager/app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'last_name',
         'email',
         'password',
     ];
@@ -30,7 +31,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
